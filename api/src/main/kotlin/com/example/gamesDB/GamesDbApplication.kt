b20b062a88a3b2ba614ddc6956e553eed6dc01bd
13197963 <46889718+13197963@users.noreply.github.com>
2019-07-20 10:56:13 +1000

    Add spring boot files

---
+++ api/src/main/kotlin/com/example/gamesDB/GamesDbApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gamesDB
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamesDbApplication
+
+fun main(args: Array<String>) {
+	runApplication<GamesDbApplication>(*args)
+}
